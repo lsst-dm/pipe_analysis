6d392c00ff045b722620c15b18a5652f22232b1d
Lauren MacArthur <lauren@astro.princeton.edu>
2020-03-06 14:24:56 -0600

    WIP: add CompareForcedVisitAnalysis task

--- python/lsst/pipe/analysis/forcedVisitAnalysis.py
+++ python/lsst/pipe/analysis/forcedVisitAnalysis.py
@@ -13,7 +13,9 @@ from lsst.pipe.base import ArgumentParser, TaskRunner, TaskError
 from lsst.meas.base.forcedPhotCcd import PerTractCcdDataIdContainer
 from lsst.afw.table.catalogMatches import matchesToCatalog
 from .analysis import Analysis
-from .visitAnalysis import VisitAnalysisConfig, VisitAnalysisTask, CompareVisitAnalysisTask, CcdAnalysis
+from .visitAnalysis import (VisitAnalysisConfig, VisitAnalysisTask, VisitAnalysisRunner,
+                            CompareVisitAnalysisTask, CompareVisitAnalysisConfig, CompareVisitAnalysisRunner,
+                            CcdAnalysis)
 from .utils import (Filenamer, AngularDistance, concatenateCatalogs, addApertureFluxesHSC, addFpPoint,
                     addFootprintNPix, addRotPoint, makeBadArray, addIntFloatOrStrColumn,
                     backoutApCorr, matchNanojanskyToAB, andCatalog, writeParquet,
@@ -236,3 +238,220 @@ class ForcedVisitAnalysisTask(VisitAnalysisTask):
                             self.plotMatches(matches, repoInfo.filterName, filenamer, repoInfo.dataId,
                                              matchRadius=self.matchRadius,
                                              matchRadiusUnitStr=self.matchRadiusUnitStr, **plotKwargs)
+
+
+class CompareForcedVisitAnalysisConfig(CompareVisitAnalysisConfig, ForcedVisitAnalysisConfig):
+
+    def setDefaults(self):
+        CompareVisitAnalysisConfig.setDefaults(self)
+        ForcedVisitAnalysisConfig.setDefaults(self)
+        self.doApplyExternalPhotoCalib1 = False
+        self.doApplyExternalPhotoCalib2 = False
+        self.doApplyExternalSkyWcs1 = False
+        self.doApplyExternalSkyWcs2 = False
+        self.matchRadiusRaDec = 0.02  # These are matched forced catalogs
+
+class CompareForcedVisitAnalysisTask(CompareVisitAnalysisTask):
+    ConfigClass = CompareForcedVisitAnalysisConfig
+    RunnerClass = CompareVisitAnalysisRunner
+    _DefaultName = "compareForcedVisitAnalysis"
+
+    @classmethod
+    def _makeArgumentParser(cls):
+        parser = ArgumentParser(name=cls._DefaultName)
+        parser.add_argument("--rerun2", required=True, help="Second rerun, for comparison")
+        parser.add_id_argument("--id", "forced_src", help="data ID with raw CCD keys, "
+                               "e.g. --id visit=12345 ccd=6^8..11", ContainerClass=PerTractCcdDataIdContainer)
+        parser.add_argument("--tract", type=str, default=None,
+                            help="Tract(s) to use (do one at a time for overlapping) e.g. 1^5^0")
+        parser.add_argument("--subdir", type=str, default="",
+                            help=("Subdirectory below plots/filter/tract-NNNN/forcedVisit-NNNN (useful "
+                                  "for, e.g., subgrouping of CCDs.  Ignored if only one CCD is "
+                                  "specified, in which case the subdir is set to ccd-NNN"))
+        return parser
+
+    def runDataRef(self, dataRefList1, dataRefList2, tract=None, subdir=""):
+        if tract is None:
+            tractList = [0, ]
+        else:
+            tractList = [int(tractStr) for tractStr in tract.split('^')]
+        self.log.debug("tractList = {}".format(tractList))
+        dataRefListPerTract1 = [None]*len(tractList)
+        dataRefListPerTract2 = [None]*len(tractList)
+        for i, tract in enumerate(tractList):
+            dataRefListPerTract1[i] = [dataRef1 for dataRef1 in dataRefList1 if
+                                       dataRef1.dataId["tract"] == tract]
+            dataRefListPerTract2[i] = [dataRef2 for dataRef2 in dataRefList2 if
+                                       dataRef2.dataId["tract"] == tract]
+        if len(dataRefListPerTract1) != len(dataRefListPerTract2):
+            raise TaskError("Lengths of comparison dataRefLists do not match!")
+        commonZpDone = True
+
+        i = -1
+        for dataRefListTract1, dataRefListTract2 in zip(dataRefListPerTract1, dataRefListPerTract2):
+            i += 1
+            if not dataRefListTract1:
+                self.log.info("No data found in --rerun for tract: {:d}".format(tractList[i]))
+                continue
+            if not dataRefListTract2:
+                self.log.info("No data found in --rerun2 for tract: {:d}".format(tractList[i]))
+                continue
+            # Get a butler and dataId for each dataset.  Needed for feeding a butler and camera into the
+            # plotting functions (for labelling the camera and plotting ccd outlines) in addition to
+            # determining if the data were processed with the HSC stack.  We assume all processing in a
+            # given rerun is self-consistent, so only need one valid dataId per comparison rerun.
+            repoInfo1 = getRepoInfo(dataRefListTract1[0],
+                                    doApplyExternalPhotoCalib=self.config.doApplyExternalPhotoCalib1,
+                                    externalPhotoCalibName=self.config.externalPhotoCalibName1,
+                                    doApplyExternalSkyWcs=self.config.doApplyExternalSkyWcs1,
+                                    externalSkyWcsName=self.config.externalSkyWcsName1)
+            repoInfo2 = getRepoInfo(dataRefListTract2[0],
+                                    doApplyExternalPhotoCalib=self.config.doApplyExternalPhotoCalib2,
+                                    externalPhotoCalibName=self.config.externalPhotoCalibName2,
+                                    doApplyExternalSkyWcs=self.config.doApplyExternalSkyWcs2,
+                                    externalSkyWcsName=self.config.externalSkyWcsName2)
+            repoInfo1.catDataset = "forced_" + repoInfo1.catDataset
+            repoInfo2.catDataset = "forced_" + repoInfo2.catDataset
+
+            fullCameraCcdList1 = getCcdNameRefList(dataRefListTract1)
+
+            ccdListPerTract1 = getDataExistsRefList(dataRefListTract1, repoInfo1.catDataset)
+            ccdListPerTract2 = getDataExistsRefList(dataRefListTract2, repoInfo2.catDataset)
+            if not ccdListPerTract1:
+                raise RuntimeError(f"No datasets found for datasetType = {repoInfo1.catDataset}")
+            if not ccdListPerTract2:
+                raise RuntimeError(f"No datasets found for datasetType = {repoInfo2.catDataset}")
+
+            if self.config.doApplyExternalPhotoCalib1:
+                ccdPhotoCalibListPerTract1 = getDataExistsRefList(dataRefListTract1,
+                                                                  repoInfo1.photoCalibDataset)
+                if not ccdPhotoCalibListPerTract1:
+                    self.log.fatal(f"No data found for {repoInfo1.photoCalibDataset} dataset...are you "
+                                   "sure you ran the external calibration?  If not, run with "
+                                   "--config doApplyExternalPhotoCalib1=False")
+            if self.config.doApplyExternalPhotoCalib2:
+                ccdPhotoCalibListPerTract2 = getDataExistsRefList(dataRefListTract2,
+                                                                  repoInfo2.photoCalibDataset)
+                if not ccdPhotoCalibListPerTract2:
+                    self.log.fatal(f"No data found for {repoInfo2.photoCalibDataset} dataset...are you "
+                                   "sure you ran the external calibration?  If not, run with "
+                                   "--config doApplyExternalPhotoCalib2=False")
+
+            ccdIntersectList = list(set(ccdListPerTract1).intersection(set(ccdListPerTract2)))
+            self.log.info("tract: {:d}".format(repoInfo1.dataId["tract"]))
+            self.log.info(f"ccdListPerTract1: \n{ccdListPerTract1}")
+            self.log.info(f"ccdListPerTract2: \n{ccdListPerTract2}")
+            self.log.info(f"ccdIntersectList: \n{ccdIntersectList}")
+            if self.config.doApplyExternalPhotoCalib1:
+                self.log.info(f"ccdPhotoCalibListPerTract1: \n{ccdPhotoCalibListPerTract1}")
+            if self.config.doApplyExternalPhotoCalib2:
+                self.log.info(f"ccdPhotoCalibListPerTract2: \n{ccdPhotoCalibListPerTract2}")
+
+            doReadFootprints = None
+            if self.config.doPlotFootprintNpix:
+                doReadFootprints = "light"
+            # Set some aliases for differing schema naming conventions
+            aliasDictList = [self.config.flagsToAlias, ]
+            if (repoInfo1.hscRun or repoInfo2.hscRun) and self.config.srcSchemaMap is not None:
+                aliasDictList += [self.config.srcSchemaMap]
+            commonZpCat1, catalog1, commonZpCat2, catalog2 = (
+                self.readCatalogs(dataRefListTract1, dataRefListTract2, "forced_src", repoInfo1, repoInfo2,
+                                  doReadFootprints=doReadFootprints, aliasDictList=aliasDictList,
+                                  excludePrefixStr=self.config.excludePrefixStr))
+
+            # Set boolean arrays indicating sources deemed unsuitable for qa analyses
+            bad1 = makeBadArray(catalog1, flagList=["base_PixelFlags_flag_edge"],
+                                onlyReadStars=self.config.onlyReadStars)
+            bad2 = makeBadArray(catalog2, flagList=["base_PixelFlags_flag_edge"],
+                                onlyReadStars=self.config.onlyReadStars)
+            badCommonZp1 = makeBadArray(commonZpCat1, flagList=None, onlyReadStars=self.config.onlyReadStars)
+            badCommonZp2 = makeBadArray(commonZpCat2, flagList=None, onlyReadStars=self.config.onlyReadStars)
+            print(sum(bad1), sum(bad2))
+            blendednessMax = 0.1
+            blendednessStr = catalog1.schema.extract(self.config.excludePrefixStr + "base_Blendedness_abs")
+            if not blendednessStr:
+                blendednessStr = catalog1.schema.extract(
+                    self.config.excludePrefixStr + "base_Blendedness_raw")
+            if blendednessStr:
+                blendednessStr = list(blendednessStr.keys())[0]
+                # Exclude blended objects
+                if blendednessStr in catalog1.schema:
+                    bad1 |= catalog1[blendednessStr] > blendednessMax
+                    badCommonZp1 |= catalog1[blendednessStr] > blendednessMax
+                if blendednessStr in catalog2.schema:
+                    bad2 |= catalog2[blendednessStr] > blendednessMax
+                    badCommonZp2 |= catalog2[blendednessStr] > blendednessMax
+            self.zpLabel = "forcedCcd\nblendedness <= " + str(blendednessMax)
+
+            # purge the catalogs of flagged sources
+            catalog1 = catalog1[~bad1].copy(deep=True)
+            catalog2 = catalog2[~bad2].copy(deep=True)
+            commonZpCat1 = commonZpCat1[~badCommonZp1].copy(deep=True)
+            commonZpCat2 = commonZpCat2[~badCommonZp2].copy(deep=True)
+
+            self.log.info("\nNumber of sources in catalogs: first = {0:d} and second = {1:d}".format(
+                          len(catalog1), len(catalog2)))
+            commonZpCat = self.matchCatalogs(commonZpCat1, commonZpCat2, matchRadius=self.matchRadius,
+                                             matchControl=self.matchControl)
+            catalog = self.matchCatalogs(catalog1, catalog2, matchRadius=self.matchRadius,
+                                         matchControl=self.matchControl)
+            # Set some aliases for differing schema naming conventions
+            if aliasDictList:
+                for cat in [commonZpCat, catalog]:
+                    cat = setAliasMaps(cat, aliasDictList)
+
+            self.log.info("Number of matches (maxDist = {0:.2f}{1:s}) = {2:d}".format(
+                          self.matchRadius, self.matchRadiusUnitStr, len(catalog)))
+
+            subdir = "ccd-" + str(ccdListPerTract1[0]) if len(ccdIntersectList) == 1 else subdir
+            filenamer = Filenamer(repoInfo1.butler, "plotCompareForcedVisit", repoInfo1.dataId, subdir=subdir)
+            hscRun = repoInfo1.hscRun if repoInfo1.hscRun else repoInfo2.hscRun
+
+            # Dict of all parameters common to plot* functions
+            tractInfo1 = repoInfo1.tractInfo if self.config.doApplyExternalPhotoCalib1 else None
+            tractInfo2 = repoInfo2.tractInfo if self.config.doApplyExternalPhotoCalib2 else None
+            tractInfo = tractInfo1 if (tractInfo1 or tractInfo2) else None
+            # Always highlight points with x-axis flag set (for cases where
+            # they do not get explicitly filtered out).
+            highlightList = [(self.config.analysis.fluxColumn.replace("_instFlux", "_flag"), 0,
+                              "turquoise"), ]
+            plotKwargs1 = dict(butler=repoInfo1.butler, camera=repoInfo1.camera, hscRun=hscRun,
+                               matchRadius=self.matchRadius, matchRadiusUnitStr=self.matchRadiusUnitStr,
+                               zpLabel=self.zpLabel, tractInfo=tractInfo, highlightList=highlightList)
+
+            if self.config.doPlotFootprintNpix:
+                self.plotFootprint(catalog, filenamer, repoInfo1.dataId, ccdList=ccdIntersectList,
+                                   **plotKwargs1)
+
+            # Create mag comparison plots using common ZP
+            if not commonZpDone:
+                zpLabel = "common (" + str(self.config.analysis.commonZp) + ")"
+                try:
+                    zpLabel = zpLabel + " " + self.catLabel
+                except Exception:
+                    pass
+                plotKwargs1.update(dict(zpLabel=zpLabel))
+                self.plotMags(commonZpCat, filenamer, repoInfo1.dataId, ccdList=fullCameraCcdList1,
+                              fluxToPlotList=["base_GaussianFlux", "base_CircularApertureFlux_12_0"],
+                              postFix="_commonZp", **plotKwargs1)
+                commonZpDone = True
+
+            plotKwargs1.update(dict(zpLabel=self.zpLabel))
+            if self.config.doPlotMags:
+                self.plotMags(catalog, filenamer, repoInfo1.dataId, ccdList=ccdIntersectList, **plotKwargs1)
+            if self.config.doPlotSizes:
+                if ("first_base_SdssShape_psf_xx" in catalog.schema and
+                        "second_base_SdssShape_psf_xx" in catalog.schema):
+                    self.plotSizes(catalog, filenamer, repoInfo1.dataId, ccdList=ccdIntersectList,
+                                   **plotKwargs1)
+                else:
+                    self.log.warn("Cannot run plotSizes: base_SdssShape_psf_xx not in catalog.schema")
+            if self.config.doApCorrs:
+                self.plotApCorrs(catalog, filenamer, repoInfo1.dataId, ccdList=ccdIntersectList,
+                                 **plotKwargs1)
+            if self.config.doPlotCentroids:
+                self.plotCentroids(catalog, filenamer, repoInfo1.dataId, ccdList=ccdIntersectList,
+                                   **plotKwargs1)
+            if self.config.doPlotStarGalaxy:
+                self.plotStarGal(catalog, filenamer, repoInfo1.dataId, ccdList=ccdIntersectList,
+                                 **plotKwargs1)
